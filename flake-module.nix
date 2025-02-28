40ada1fc86f68c4e78496126fb0ff67617b0168d
Neochaotics <neochaotics@pm.me>
2025-02-28 11:15:15 -0500

    init

---
+++ flake-module.nix
@@ -0,0 +1,55 @@
+_localFlake:
+{ inputs, ... }:
+{
+  imports = [
+    inputs.treefmt-nix.flakeModule
+    inputs.devshell.flakeModule
+    inputs.flake-root.flakeModule
+    inputs.git-hooks-nix.flakeModule
+  ];
+
+  perSystem =
+    { config, pkgs, ... }:
+    {
+      treefmt.config = {
+        inherit (config.flake-root) projectRootFile;
+        flakeCheck = false;
+        programs = {
+          nixfmt = {
+            enable = true;
+            package = pkgs.nixfmt-rfc-style;
+          };
+          statix.enable = true;
+          deadnix.enable = true;
+
+          actionlint.enable = true;
+          mdformat.enable = true;
+          yamlfmt.enable = true;
+          shfmt.enable = true;
+        };
+      };
+
+      devshells.default = {
+        name = "NixOS Utility Environment";
+        motd = "";
+        packages = [
+          pkgs.nil
+          pkgs.nixd
+          pkgs.sops
+          config.treefmt.build.wrapper
+        ] ++ (pkgs.lib.attrValues config.treefmt.build.programs);
+      };
+
+      # Git pre-commit hooks
+      pre-commit.settings.hooks = {
+        treefmt = {
+          enable = true;
+          package = config.treefmt.build.wrapper;
+        };
+        statix = {
+          enable = true;
+          package = config.treefmt.build.programs.statix;
+        };
+      };
+    };
+}
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "FREEDPOM FORMAT";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell"; # Development environment
+    flake-parts.url = "github:hercules-ci/flake-parts"; # Flake infrastructure
+    flake-root.url = "github:srid/flake-root"; # Flake infrastructure
+    git-hooks-nix.url = "github:cachix/git-hooks.nix"; # Git pre-commit hooks
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Nixpkgs
+    treefmt-nix.url = "github:numtide/treefmt-nix"; # Code formatting and linting
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, flake-parts-lib, ... }:
+      let
+        inherit (flake-parts-lib) importApply;
+        flakeModules.default = importApply ./flake-module.nix { inherit withSystem; };
+      in
+      {
+        imports = [
+          flakeModules.default
+          inputs.flake-parts.flakeModules.flakeModules
+        ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ];
+        flake = {
+          inherit flakeModules;
+        };
+      }
+    );
+}
