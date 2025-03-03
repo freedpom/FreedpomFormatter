f54f16208b05622cb9ed066a98ab1b903920ddd7
yung-gimp <>
2025-03-02 22:19:47 -0500

    flakeModule :)

--- flake.nix
+++ flake.nix
@@ -2,28 +2,46 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    devshell.url = "github:numtide/devshell";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+
+    # Flake infrastructure
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, flake-parts-lib, config,... }:
-    let
-      inherit (flake-parts-lib) importApply;
-      flakeModules.default = importApply ./flake-module.nix { inherit withSystem inputs config; };
-    in {
-      imports = [
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        withSystem,
+        flake-parts-lib,
+        config,
+        ...
+      }:
+      let
+        inherit (flake-parts-lib) importApply;
+        flakeModules.default = importApply ./flake-module.nix { inherit withSystem inputs config; };
+      in
+      {
+        imports = [
+          flakeModules.default
+        ];
 
-      ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        perSystem =
+          _:
+          {
 
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-
-      };
-      flake = {
-        inherit flakeModules;
-      };
-    });
+          };
+        flake = {
+          inherit flakeModules;
+        };
+      }
+    );
 }
