265a89b5808727949e13b552690f6629c4b6c903
yung-gimp <>
2025-03-02 22:19:47 -0500

    flakeModule :)

--- flake-module.nix
+++ flake-module.nix
@@ -1,39 +1,62 @@
 localFlake:
 
-{ lib, config, self, pkgs, ... }:
+{ ... }:
 {
   imports = [
     localFlake.inputs.treefmt-nix.flakeModule
     localFlake.inputs.devshell.flakeModule
     localFlake.inputs.flake-root.flakeModule
     localFlake.inputs.git-hooks-nix.flakeModule
-    localFlake.inputs.home-manager.flakeModules.home-manager
   ];
-  perSystem = { system, ... }: {
-    treefmt.config = {
-      inherit (config.flake-root) projectRootFile;
-      flakeCheck = false;
-      programs = {
-        # Nix formatting tools
-        nixfmt = {
-          enable = true;
-          package = pkgs.nixfmt-rfc-style;
+  perSystem =
+    { pkgs, config, ... }:
+    {
+      treefmt.config = {
+        inherit (config.flake-root) projectRootFile;
+        flakeCheck = false;
+        programs = {
+          # Nix formatting tools
+          nixfmt = {
+            enable = true;
+            package = pkgs.nixfmt-rfc-style;
+          };
+          statix.enable = true; # Static analysis for Nix
+          deadnix.enable = true; # Detect dead code in Nix
+
+          typos.enable = true;
+          typos.excludes = [
+            "*.png"
+            "*.yaml"
+          ];
+
+          # Additional formatters
+          actionlint.enable = true; # GitHub Actions linter
+          mdformat.enable = true; # Markdown formatter
+          yamlfmt.enable = true;
+          shfmt.enable = true;
         };
-        statix.enable = true; # Static analysis for Nix
-        deadnix.enable = true; # Detect dead code in Nix
+      };
+      # Development environment configuration
+      devshells.default = {
+        name = "NIX UTILITY ENV";
+        motd = ""; # Message of the day
+        packages = [
+          pkgs.rage
+          pkgs.sops
+          config.treefmt.build.wrapper
+        ] ++ (pkgs.lib.attrValues config.treefmt.build.programs);
+      };
 
-        typos.enable = true;
-        typos.excludes = [
-          "*.png"
-          "*.yaml"
-        ];
-
-        # Additional formatters
-        actionlint.enable = true; # GitHub Actions linter
-        mdformat.enable = true; # Markdown formatter
-        yamlfmt.enable = true;
-        shfmt.enable = true;
+      # Git pre-commit hooks
+      pre-commit.settings.hooks = {
+        treefmt = {
+          enable = true;
+          package = config.treefmt.build.wrapper;
+        };
+        statix = {
+          enable = true;
+          package = config.treefmt.build.programs.statix;
+        };
       };
     };
-  };
 }
