7f3a80bbe57791d26065b0cfa0082e311e4957c1
yung-gimp <>
2025-03-02 21:55:33 -0500

    init

--- flake-module.nix
+++ flake-module.nix
@@ -1,66 +1,39 @@
+localFlake:
+
+{ lib, config, self, pkgs, ... }:
 {
-  lib,
-  flake-parts-lib,
-  ...
-}:
-let
-  inherit (flake-parts-lib)
-    mkPerSystemOption
-    ;
-in
-
-{
-  options = {
-    perSystem = mkPerSystemOption (
-      {
-        config,
-        pkgs,
-        ...
-      }:
-      {
-        config = {
-          treefmt.config = {
-            inherit (config.flake-root) projectRootFile;
-            flakeCheck = false;
-            programs = {
-              nixfmt = {
-                enable = true;
-                package = pkgs.nixfmt-rfc-style;
-              };
-              statix.enable = true;
-              deadnix.enable = true;
-
-              actionlint.enable = true;
-              mdformat.enable = true;
-              yamlfmt.enable = true;
-              shfmt.enable = true;
-            };
-          };
-
-          devshells.default = {
-            name = "NixOS Utility Environment";
-            motd = "";
-            packages = [
-              pkgs.nil
-              pkgs.nixd
-              pkgs.sops
-              config.treefmt.build.wrapper
-            ] ++ (pkgs.lib.attrValues config.treefmt.build.programs);
-          };
-
-          # Git pre-commit hooks
-          pre-commit.settings.hooks = {
-            treefmt = {
-              enable = true;
-              package = config.treefmt.build.wrapper;
-            };
-            statix = {
-              enable = true;
-              package = config.treefmt.build.programs.statix;
-            };
-          };
+  imports = [
+    localFlake.inputs.treefmt-nix.flakeModule
+    localFlake.inputs.devshell.flakeModule
+    localFlake.inputs.flake-root.flakeModule
+    localFlake.inputs.git-hooks-nix.flakeModule
+    localFlake.inputs.home-manager.flakeModules.home-manager
+  ];
+  perSystem = { system, ... }: {
+    treefmt.config = {
+      inherit (config.flake-root) projectRootFile;
+      flakeCheck = false;
+      programs = {
+        # Nix formatting tools
+        nixfmt = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
         };
-      }
-    );
+        statix.enable = true; # Static analysis for Nix
+        deadnix.enable = true; # Detect dead code in Nix
+
+        typos.enable = true;
+        typos.excludes = [
+          "*.png"
+          "*.yaml"
+        ];
+
+        # Additional formatters
+        actionlint.enable = true; # GitHub Actions linter
+        mdformat.enable = true; # Markdown formatter
+        yamlfmt.enable = true;
+        shfmt.enable = true;
+      };
+    };
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,40 +1,29 @@
 {
-  description = "FREEDPOM FORMAT";
+  description = "Description for the project";
 
   inputs = {
-    devshell.url = "github:numtide/devshell"; # Development environment
-    flake-parts.url = "github:hercules-ci/flake-parts"; # Flake infrastructure
-    flake-root.url = "github:srid/flake-root"; # Flake infrastructure
-    git-hooks-nix.url = "github:cachix/git-hooks.nix"; # Git pre-commit hooks
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Nixpkgs
-    treefmt-nix.url = "github:numtide/treefmt-nix"; # Code formatting and linting
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      let
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, flake-parts-lib, config,... }:
+    let
+      inherit (flake-parts-lib) importApply;
+      flakeModules.default = importApply ./flake-module.nix { inherit withSystem inputs config; };
+    in {
+      imports = [
 
-        flakeModules.default = {
-          imports = [ ./flake-module.nix ];
-        };
+      ];
 
-      in
-      {
-        imports = [
-          flakeModules.default
-          inputs.flake-parts.flakeModules.flakeModules
-          inputs.treefmt-nix.flakeModule
-          inputs.devshell.flakeModule
-          inputs.flake-root.flakeModule
-          inputs.git-hooks-nix.flakeModule
-        ];
-        systems = [
-          "x86_64-linux"
-        ];
-        flake = {
-          inherit flakeModules;
-        };
-      }
-    );
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+      };
+      flake = {
+        inherit flakeModules;
+      };
+    });
 }
